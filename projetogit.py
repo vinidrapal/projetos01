6f6f4032f181ca8dbf942177b7466d3f15b402c2
Vinicius Drapal <vinicius_drapal@hotmail.com>
2023-11-26 14:16:58 -0300

    AUTOMAÇÃO MOUSE

--- projetogit.py
+++ projetogit.py
@@ -1,1 +1,8 @@
-print ("Ola git")
+import pyautogui
+
+pyautogui.moveTo(x=224,y=212,duration=2)
+pyautogui.click(button='right')
+pyautogui.moveTo(x=345,y=376,duration=2)
+pyautogui.click(button='left')
+pyautogui.moveTo(x=612,y=410,duration=2)
+pyautogui.click(button='left')
